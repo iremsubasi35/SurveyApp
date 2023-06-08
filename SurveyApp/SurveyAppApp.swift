784b2266db484bd91ef0051ac4e4a110fdf68caa
Irem Subası <irem.subasi@rapsodo.com>
2023-06-08 16:01:32 +0300

    Initial Commit

---
+++ SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Irem Subası on 8.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
